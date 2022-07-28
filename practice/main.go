d2a0df08dc738fb2f4d08f67ab1bfb2b1161ad68
Sofia Jones <sofjones@Sofias-MacBook-Pro.local>
2022-07-28 15:24:05 -0700

    commit change

--- practice/main.go
+++ practice/main.go
@@ -6,4 +6,13 @@ import (
 
 func main() {
 	fmt.Println("Pointers")
+	anInt := 4
+	var p = &anInt
+
+	fmt.Println(&anInt)
+	fmt.Println(*p)
+	*p = anInt / 2
+	fmt.Println("set pointer value to half anInt value", *p)
+	fmt.Println(anInt) //anInt value is changed
+	//fmt.Printf("%d",anInt)
 }
