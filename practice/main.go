a3c0fbb05242e38de1650e8369dab4398250e9ef
sofjones <sofjones@github.com>
2022-07-29 11:25:24 -0700

    switch statements

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-func main() {
 
-	fmt.Println("Arrays")
+func main() {
+	rand.Seed(time.Now().Unix())
+	dayOfWeek := rand.Intn(7) + 1
+	fmt.Println("day of week: ", dayOfWeek)	
+	var result string
+	switch dayOfWeek {
+	case 1:
+		result = "It's sunday!"
+	case 2:
+		result = "it's monday"
+	case 3:
+		result = "its tuesday"
+	default:
+		result = "another day"
+	}
+	fmt.Println(result)
 
 }
